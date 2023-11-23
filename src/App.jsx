ba22271cd4390ddfb1eb7e4bce6abcaf2ed1ed3d
mToan <mto.work@outlook.com>
2023-11-23 10:17:57 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,50 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import './App.scss'
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <div className="card-header">
+          <img src="https://placehold.co/600x400" alt="img" />
+        </div>
+        
+        <div className="card-body">
+          <h1 className='title'>Title</h1>
+
+          <div className="rating">
+            <div className="score">  
+              <i className="fa-solid fa-star"></i>
+              <i className="fa-solid fa-star"></i>
+              <i className="fa-solid fa-star"></i>
+            </div>
+            <div className="note">4(999)</div>
+          </div>
+
+          <div className="content">
+            <div className="content-title">Secondary text</div>
+            <div className="content-body">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias non quis laborum aliquid temporibus, ratione ipsum? Cum ipsam dolorum iusto culpa. Quasi quos voluptate impedit quam rem maiores aliquam a.
+            </div>
+          </div>
+
+          <div className="card-footer">
+            <div className="subtitle">Subtitle</div>
+            <div className="items">
+              <button>Item 1</button>
+              <button>Item 1</button>
+              <button>Item 1</button>
+              <button>Item 1</button>
+            </div>
+            <div className="action">ACTION</div>
+          </div>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
